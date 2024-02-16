0f330c1d8a5927d987d01a4679586158b9bec088
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2024-02-16 11:33:39 -0600

    🔧 refactor(playwright.ts): rename WebBrowser class to WebBrowserManager for better clarity and semantics 🔧 refactor(playwright.ts): rename methods getBrowser and getPage to createBrowserInstance and createPage respectively for better clarity and semantics 🔧 refactor(playwright.ts): change return type of launchBrowser method to Promise<WebComponents | LaunchBrowserError> to handle potential launch errors 🆕 feat(web_errors.ts): add LaunchBrowserError class to handle errors that occur during browser launch or page creation

--- src/config/playwright.ts
+++ src/config/playwright.ts
@@ -4,30 +4,37 @@ import {
   type Browser,
   type BrowserType,
 } from 'playwright';
+import {LaunchBrowserError} from '../errors/web_errors';
 type WebComponents = {browser: Browser; page: Page};
-export default class WebBrowser {
+export default class WebBrowserManager {
   private launchOptions: LaunchOptions;
   private browserType: BrowserType;
   constructor(browser: BrowserType, launchOptions: LaunchOptions = {}) {
     this.browserType = browser;
     this.launchOptions = launchOptions;
   }
-  async launchBrowser(): Promise<WebComponents> {
+  async launchBrowser(): Promise<WebComponents | LaunchBrowserError> {
     try {
-      const browser = await this.getBrowser();
-      const page = await this.getPage(browser);
+      const browser = await this.createBrowserInstance();
+      const page = await this.createPage(browser);
       return {browser, page};
     } catch (error) {
-      console.error('🚀 ~ Browser ~ launchBrowser ~ error:', error);
-      throw error;
+      const launchError = new LaunchBrowserError(
+        'An error occurred while launching the browser or create the page'
+      );
+      console.error(
+        '🚀 ~ WebBrowserManager ~ launchBrowser ~ launchError:',
+        launchError.message
+      );
+      return launchError;
     }
   }
 
-  private async getBrowser(): Promise<Browser> {
+  private async createBrowserInstance(): Promise<Browser> {
     return this.browserType.launch(this.launchOptions);
   }
 
-  private async getPage(browser: Browser): Promise<Page> {
+  private async createPage(browser: Browser): Promise<Page> {
     const page = await browser.newPage();
     return page;
   }
