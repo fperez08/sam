051b705175944ee5bdee4ea64daa8734a44a7c08
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2024-02-19 09:48:35 -0600

    🔥 chore(index.ts): remove unused code for taking a screenshot of SAMS home page
    🔄 chore(package.json): update dependencies and remove unused dependencies
    
    🔥 chore(playwright.ts): remove unused file and class WebBrowserManager
    
    🔥 chore(web_errors.ts): remove unused error class LaunchBrowserError

--- index.ts
+++
@@ -1,8 +0,0 @@
-import ChromeBrowser from './src/config/playwright';
-import {SAMS_HOME_URL} from './src/constants/urls';
-const chrome = new ChromeBrowser();
-chrome.init().then(async ({browser, page}) => {
-  await page.goto(SAMS_HOME_URL);
-  await page.screenshot({path: 'sams.png'});
-  await browser.close();
-});
