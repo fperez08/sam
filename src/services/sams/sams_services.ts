d8b98e35a0c277b864a69efa20d3ad04c5188ace
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2024-02-20 12:31:02 -0600

    🔀 refactor(sams_services.ts): add return type to getSales() method to improve code readability and maintainability

--- src/services/sams/sams_services.ts
+++ src/services/sams/sams_services.ts
@@ -1,11 +1,11 @@
-import type {AxiosRequestConfig} from 'axios';
+import type {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios';
 import HttpClient from '../../http/http_client';
 
 export default class SamsService extends HttpClient {
   constructor(config: AxiosRequestConfig) {
     super(config);
   }
-  public getSales() {
+  public getSales(): Promise<AxiosResponse | AxiosError> {
     return this.get('sams/department/rebajas/_/N-akm');
   }
 }
