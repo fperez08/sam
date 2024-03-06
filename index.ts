fe02bac1e5ead2d912ca9b31091ffda34e0e394a
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2024-03-06 16:36:43 -0600

    🔧 fix(index.ts): import EmailService and generateHtmlTable functions to enable sending emails with sale items table ✨ feat(email_service.ts): add EmailService class to handle sending emails using nodemailer ✨ feat(html.ts): add generateHtmlTable function to generate an HTML table from a JSON array 🔧 fix(string.ts): add revertCamelCase function to reverse the casing of a string for table headers

--- index.ts
+++ index.ts
@@ -5,12 +5,30 @@ import SamsService from './src/services/sams/sams_services';
 import {extractDataFromArray} from './src/utils/data_utils';
 import {SAMS_SERVICE_CONFIG} from './src/config/sams_request_config';
 import {DBService} from './src/database/databse_service';
+import EmailService from './src/services/email_service';
+import {generateHtmlTable} from './src/utils/html';
+import type Mail from 'nodemailer/lib/mailer';
 const jp = require('jsonpath');
 require('dotenv').config();
 const salesQuery = require('./src/data/sams_sales_query.json');
 
 const samsService = new SamsService(SAMS_SERVICE_CONFIG);
 const samsData = new SamsDataManager();
+const transporterConfig = {
+  service: 'Gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.FROM_EMAIL,
+    pass: process.env.FROM_EMAIL_PASSWORD,
+  },
+};
+const mailOptions: Mail.Options = {
+  from: process.env.FROM_EMAIL,
+  to: process.env.TO_EMAIL,
+  subject: 'Products on Sale',
+};
 console.log('Starting Sams WebScraper...');
 
 const response = await samsService.getSales();
@@ -32,6 +50,26 @@ if ((response as AxiosResponse).data) {
   const finalSalesData = samsData.convertItemTimeStampToDate(
     discountedData.filter(item => item.saleExpiresAt)
   );
+  const saleItemsWithHigDiscount = finalSalesData.filter(saleItem => {
+    if (saleItem.discount) {
+      return parseInt(saleItem.discount) >= 20;
+    }
+  });
+  if (saleItemsWithHigDiscount.length > 0) {
+    const table = generateHtmlTable(saleItemsWithHigDiscount, [
+      'name',
+      'displayName',
+      'lastPrice',
+      'finalPrice',
+      'saleExpiresAt',
+      'discount',
+      'priceDifference',
+    ]);
+    mailOptions.html = table;
+    console.log('Sending email...');
+    const emailService = new EmailService(transporterConfig);
+    emailService.sendEmail(mailOptions);
+  }
   console.log('Saving the data: ', finalSalesData);
   DBService.storeSalesData(1, finalSalesData);
 } else {
---
+++ src/utils/html.ts
@@ -0,0 +1,39 @@
+import {revertCamelCase} from './string';
+
+/**
+ * Generate an HTML table from a JSON array.
+ * @param {{[key:string]:any}} jsonArray - an array of JSON objects
+ * @param {string[]} headers - an array of column headers
+ * @returns an HTML table as a string
+ */
+export function generateHtmlTable(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  jsonArray: {[key: string]: any}[],
+  headers: string[] = []
+): string {
+  if (jsonArray.length === 0) {
+    return '';
+  }
+
+  const tableHeaders = headers.length > 0 ? headers : Object.keys(jsonArray[0]);
+  let htmlTable = '<table style="border-collapse: collapse; width: 100%;">';
+
+  // Table headers
+  htmlTable += '<tr>';
+  for (const header of tableHeaders) {
+    htmlTable += `<th style="border: 1px solid #dddddd; padding: 8px;">${revertCamelCase(header)}</th>`;
+  }
+  htmlTable += '</tr>';
+
+  // Table data
+  for (const item of jsonArray) {
+    htmlTable += '<tr>';
+    for (const header of tableHeaders) {
+      htmlTable += `<td style="border: 1px solid #dddddd; padding: 8px;">${item[header]}</td>`;
+    }
+    htmlTable += '</tr>';
+  }
+
+  htmlTable += '</table>';
+  return htmlTable;
+}
