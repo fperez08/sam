526abd83f68ff439f186a14d77198325ee84a1b2
Francisco Perez <francisco.perez@Franciscos-MacBook-Pro.local>
2024-03-05 18:47:01 -0600

    🐛 fix(index.ts): fix saving the data by applying discount and converting timestamp to date ✨ feat(sams_data.ts): add functions to calculate item discount and convert timestamp to date 🔧 chore(data_utils.ts): add function to convert Unix timestamp to date string

--- src/data/sams_data.ts
+++ src/data/sams_data.ts
@@ -1,5 +1,5 @@
 import type {SaleItem, SaleItemRaw} from '../models/sams_data_models';
-import {mergeObjects} from '../utils/data_utils';
+import {mergeObjects, convertTimeStampToDate} from '../utils/data_utils';
 
 export default class SamsDataManager {
   /**
@@ -40,5 +40,53 @@ export default class SamsDataManager {
       uniqueSalesData.push(mergeObjects(data[index], data[index + 1]));
     }
     return uniqueSalesData;
+  }
+
+  /**
+   * Calculates the discount for each item in the given sales data.
+   *
+   * @param {SaleItem[]} data - Array of SalesItem objects.
+   * @returns {SaleItem[]} - The sales data with the calculated discounts
+   */
+  public calculateItemsDiscount(data: SaleItem[]): SaleItem[] {
+    const itemsWithDiscount: SaleItem[] = [];
+
+    data.forEach(item => {
+      const lastPrice = parseFloat(item.lastPrice);
+      const finalPrice = parseFloat(item.finalPrice);
+
+      const discount = (((lastPrice - finalPrice) / lastPrice) * 100).toFixed(
+        2
+      );
+      const priceDifference = (lastPrice - finalPrice).toFixed(2);
+
+      const itemWithDiscount: SaleItem = {
+        ...item,
+        discount: discount,
+        priceDifference: priceDifference,
+      };
+
+      itemsWithDiscount.push(itemWithDiscount);
+    });
+
+    return itemsWithDiscount;
+  }
+
+  /**
+   *  Converts the unix timestamp of each item to a date string.
+   *
+   * @param {SaleItem[]} data - The data to convert
+   * @returns  {SaleItem[]} - The sales data with the date string
+   */
+  public convertItemTimeStampToDate(data: SaleItem[]): SaleItem[] {
+    const itemsWithDate: SaleItem[] = [];
+    data.forEach(item => {
+      if (item.saleExpiresAt) {
+        const timeStamp = parseInt(item.saleExpiresAt);
+        item.saleExpiresAt = convertTimeStampToDate(timeStamp);
+        itemsWithDate.push(item);
+      }
+    });
+    return itemsWithDate;
   }
 }
