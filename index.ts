04f42da28eff5b9c972d2ed21126bbdb6632a146
fperez08 <francisco.pgarcia28@gmail.com>
2024-03-20 19:50:49 -0600

    ✅ (index.ts): uncomment the line to send the email 💡 (index.ts): improve logging message to indicate that there are no changes in the products

--- index.ts
+++ index.ts
@@ -47,7 +47,7 @@ for (let index = 0; index < CONFIGS.length; index++) {
     EMAIL_OPTIONS.html = table;
     console.log(`Sending email: ${subject}...`);
     const emailService = new EmailService(TRANSPORTER_CONFIG);
-    //emailService.sendEmail(EMAIL_OPTIONS);
+    emailService.sendEmail(EMAIL_OPTIONS);
   } else {
     console.log('No changes in the products: ', CONFIGS[index].name);
   }
